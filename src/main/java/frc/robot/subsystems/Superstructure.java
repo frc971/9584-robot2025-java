4df9d3b9eea39914ec39f7ef66c29ea6e6c8674d
Bang Xiao <bangxiao0927@gmail.com>
2025-11-08 17:42:49 -0800

    Fixed armup released

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -155,7 +155,7 @@ public class Superstructure extends edu.wpi.first.wpilibj2.command.SubsystemBase
     public Command ArmUpReleased() {
         return Commands.runOnce(() -> {
             System.out.println("============ Arm stopped");
-            armMotor.set(TalonSRXControlMode.PercentOutput, 0);
+            armMotor.set(ControlMode.Position, armMotor.getSelectedSensorPosition(0));
         });
     }
 
